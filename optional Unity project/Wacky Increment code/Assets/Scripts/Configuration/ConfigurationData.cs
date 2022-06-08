3da9ce30f4ca033a0eb23e55b99b8195cd67f08d
Ihor Popovych <cantachis@gmail.com>
2022-06-08 15:59:10 +0300

    Unity project codes

---
+++ optional Unity project/Wacky Increment code/Assets/Scripts/Configuration/ConfigurationData.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+/// <summary>
+/// A container for the configuration data
+/// </summary>
+public class ConfigurationData
+{
+    #region Fields
+
+    const string ConfigurationDataFileName = "ConfigurationData.csv";
+
+    // configuration data
+    float paddleMoveUnitsPerSecond = 10;
+    float ballImpulseForce = 200;
+    float ballLifeSeconds = 10;
+    float minSpawnSeconds = 5;
+    float maxSpawnSeconds = 10;
+    int ballsPerGame = 5;
+    int standardBlockPoints = 1;
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets the paddle move units per second
+    /// </summary>
+    /// <value>paddle move units per second</value>
+    public float PaddleMoveUnitsPerSecond
+    {
+        get { return paddleMoveUnitsPerSecond; }
+    }
+
+    /// <summary>
+    /// Gets the impulse force to apply to move the ball
+    /// </summary>
+    /// <value>impulse force</value>
+    public float BallImpulseForce
+    {
+        get { return ballImpulseForce; }    
+    }
+
+    /// <summary>
+    /// Gets the number of seconds the ball lives
+    /// </summary>
+    /// <value>ball life seconds</value>
+    public float BallLifeSeconds
+    {
+        get { return ballLifeSeconds; }
+    }
+
+    /// <summary>
+    /// Gets the minimum number of seconds for a ball spawn
+    /// </summary>
+    /// <value>minimum spawn seconds</value>
+    public float MinSpawnSeconds
+    {
+        get { return minSpawnSeconds; }
+    }
+
+    /// <summary>
+    /// Gets the maximum number of seconds for a ball spawn
+    /// </summary>
+    /// <value>maximum spawn seconds</value>
+    public float MaxSpawnSeconds
+    {
+        get { return maxSpawnSeconds; }
+    }
+
+    /// <summary>
+    /// Gets the number of balls per game
+    /// </summary>
+    public int BallsPerGame
+    {
+        get { return ballsPerGame; }
+    }
+
+    /// <summary>
+    /// Gets how many points a standard block is worth
+    /// </summary>
+    public int StandardBlockPoints
+    {
+        get { return standardBlockPoints; }
+    }
+
+    #endregion
+
+    #region Constructor
+
+    /// <summary>
+    /// Constructor
+    /// Reads configuration data from a file. If the file
+    /// read fails, the object contains default values for
+    /// the configuration data
+    /// </summary>
+    public ConfigurationData()
+    {
+        // read and save configuration data from file
+        StreamReader input = null;
+        try
+        {
+            // create stream reader object
+            input = File.OpenText(Path.Combine(
+                Application.streamingAssetsPath, ConfigurationDataFileName));
+
+            // read in names and values
+            string names = input.ReadLine();
+            string values = input.ReadLine();
+
+            // set configuration data fields
+            SetConfigurationDataFields(values);
+        }
+        catch (Exception e)
+        {
+        }
+        finally
+        {
+            // always close input file
+            if (input != null)
+            {
+                input.Close();
+            }
+        }
+    }
+
+    #endregion
+
+    #region Private methods
+
+    /// <summary>
+    /// Sets the configuration data fields from the provided
+    /// csv string
+    /// </summary>
+    /// <param name="csvValues">csv string of values</param>
+    void SetConfigurationDataFields(string csvValues)
+    {
+        // the code below assumes we know the order in which the
+        // values appear in the string. We could do something more
+        // complicated with the names and values, but that's not
+        // necessary here
+        string[] values = csvValues.Split(',');
+        paddleMoveUnitsPerSecond = float.Parse(values[0]);
+        ballImpulseForce = float.Parse(values[1]);
+        ballLifeSeconds = float.Parse(values[2]);
+        minSpawnSeconds = float.Parse(values[3]);
+        maxSpawnSeconds = float.Parse(values[4]);
+        ballsPerGame = int.Parse(values[5]);
+        standardBlockPoints = int.Parse(values[6]);
+    }
+
+    #endregion
+}
